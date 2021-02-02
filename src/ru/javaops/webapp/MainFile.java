3ac4881cafb86702e6379855344ee7e9835834a3
sanderman155 <kinzhitayevsayan@gmaik.com>
2021-02-02 21:26:38 +0300

    pretty print with tabulations

--- src/ru/javaops/webapp/MainFile.java
+++ src/ru/javaops/webapp/MainFile.java
@@ -4,19 +4,21 @@ import java.io.File;
 import java.util.Objects;
 
 public class MainFile {
-    public static void traverse(File file) {
+    public static void traverse(File file, StringBuilder tab) {
         if (file.exists() && !file.getName().equals(".idea") &&
             !file.getName().equals(".git")) {
-            System.out.println(file.getName());
+            System.out.println(tab + file.getName());
             if (file.isDirectory()) {
+                tab.append('\t');
                 for (File f: Objects.requireNonNull(file.listFiles())) {
-                    traverse(f);
+                    traverse(f, tab);
                 }
+                tab.deleteCharAt(tab.length() - 1);
             }
         }
     }
 
     public static void main(String[] args) {
-        traverse(new File("."));
+        traverse(new File("."), new StringBuilder());
     }
 }
