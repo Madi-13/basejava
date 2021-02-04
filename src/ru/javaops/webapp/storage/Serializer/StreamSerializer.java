4de139a59d97b51d4a07dc937d269eba063bb7b1
sanderman155 <kinzhitayevsayan@gmaik.com>
2021-02-04 20:59:34 +0300

    Serializer interface(strategy pattern)

---
+++ src/ru/javaops/webapp/storage/Serializer/StreamSerializer.java
@@ -0,0 +1,14 @@
+package ru.javaops.webapp.storage.Serializer;
+
+import ru.javaops.webapp.model.Resume;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public interface StreamSerializer {
+    void doWrite(OutputStream os, Resume resume) throws IOException;
+
+    Resume doRead(InputStream is) throws IOException;
+
+}
