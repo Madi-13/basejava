ad0aa4d20ec5c0ef935c0d537dcb88b3852c195f
sanderman155 <kinzhitayevsayan@gmaik.com>
2021-02-05 21:42:09 +0300

    renamed ObjectStreamStorage

--- test/ru/javaops/webapp/storage/ObjectFileStorageTest.java
+++ test/ru/javaops/webapp/storage/ObjectFileStorageTest.java
@@ -1,10 +1,9 @@
 package ru.javaops.webapp.storage;
-
-import ru.javaops.webapp.storage.serializer.ObjectStreamStorage;
+import ru.javaops.webapp.storage.serializer.ObjectStreamSerializer;
 
 public class ObjectFileStorageTest extends AbstractStorageTest {
     public ObjectFileStorageTest() {
-        super(new FileStorage(STORAGE_DIRECTORY, new ObjectStreamStorage()));
+        super(new FileStorage(STORAGE_DIRECTORY, new ObjectStreamSerializer()));
     }
 
 
--- test/ru/javaops/webapp/storage/ObjectPathStorageTest.java
+++ test/ru/javaops/webapp/storage/ObjectPathStorageTest.java
@@ -1,9 +1,9 @@
 package ru.javaops.webapp.storage;
 
-import ru.javaops.webapp.storage.serializer.ObjectStreamStorage;
+import ru.javaops.webapp.storage.serializer.ObjectStreamSerializer;
 
 public class ObjectPathStorageTest extends AbstractStorageTest {
     public ObjectPathStorageTest() {
-        super(new PathStorage(STORAGE_DIRECTORY.getAbsolutePath(), new ObjectStreamStorage()));
+        super(new PathStorage(STORAGE_DIRECTORY.getAbsolutePath(), new ObjectStreamSerializer()));
     }
 }
