753f5bb343dddaaf44b7bad0f01f1b73a4867d43
Sayan Kinzhitayev <kinzhitayevsayan@gmail.com>
2021-03-09 12:34:42 +0300

    add new constructor(exception e)

--- src/ru/javaops/webapp/exception/ExistStorageException.java
+++ src/ru/javaops/webapp/exception/ExistStorageException.java
@@ -4,4 +4,8 @@ public class ExistStorageException extends StorageException{
     public ExistStorageException(String uuid) {
         super("Given resume(" + uuid + ") is already in the storage", uuid);
     }
+
+    public ExistStorageException(Exception e) {
+        super("Resume already exists", e);
+    }
 }
