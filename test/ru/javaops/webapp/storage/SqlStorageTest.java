71a58894d19c6001f27e6e14f66a4200b415d5b8
Sayan Kinzhitayev <kinzhitayevsayan@gmail.com>
2021-03-09 12:36:02 +0300

    now saveExist test work correctly

--- test/ru/javaops/webapp/storage/SqlStorageTest.java
+++ test/ru/javaops/webapp/storage/SqlStorageTest.java
@@ -1,15 +1,9 @@
 package ru.javaops.webapp.storage;
 
-import ru.javaops.webapp.exception.StorageException;
-import ru.javaops.webapp.storage.serializer.SqlStorage;
 import ru.javaops.webapp.util.Config;
 
 public class SqlStorageTest extends AbstractStorageTest {
     public SqlStorageTest() {
         super(new SqlStorage(Config.get().getDbUrl(), Config.get().getDbUser(), Config.get().getDbPassword()));
-    }
-
-    @Override
-    public void saveExist() throws StorageException {
     }
 }
