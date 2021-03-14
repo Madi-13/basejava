ceecae29b8084ea2fdacaa2ebb9136b7c51b09f1
Sayan Kinzhitayev <kinzhitayevsayan@gmail.com>
2021-03-14 20:27:29 +0300

    added section table

--- config/init_db.sql
+++ config/init_db.sql
@@ -1,3 +1,33 @@
+create table resume
+(
+    uuid char(36) not null
+        constraint resume_pk
+            primary key,
+    full_name text
+);
+
+alter table resume owner to sayan;
+
+create table section
+(
+    id          serial   not null
+        constraint section_pk
+            primary key,
+    type        text     not null,
+    info        text     not null,
+    resume_uuid char(36) not null
+        constraint section_resume_uuid_fk
+            references resume
+            on update restrict
+            on delete cascade
+);
+
+create unique index section_uuid_index
+    on section(resume_uuid, type);
+
+alter table section
+    owner to sayan;
+
 create table contact
 (
     id serial not null
@@ -10,18 +40,9 @@ create table contact
     type text not null,
     value text not null
 );
+
 create unique index contact_uuid_index
 	on contact (resume_uuid, type);
 
 alter table contact owner to sayan;
-
-create table resume
-(
-    uuid char(36) not null
-        constraint resume_pk
-            primary key,
-    full_name text
-);
-
-alter table resume owner to sayan;
 
